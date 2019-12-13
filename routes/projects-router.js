3e5db127a162af8aff1346146a27b9dac9659edf
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-12-13 08:56:08 -0800

    sanity check
    server is up and running on port 9994

--- routes/projects-router.js
+++ routes/projects-router.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 const Projects = require('./projects-model.js')
 
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -6,5 +6,7 @@ const server = express();
 
 server.use(express.json());
 server.use('/api/projects', ProjectRouter);
-
+server.get('/', (req, res) => {
+    res.send('<h2>THE SERVER IS UP AND RUNNING</h2>')
+})
 module.exports = server;
