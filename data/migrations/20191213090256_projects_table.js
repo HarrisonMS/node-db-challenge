79ff98a2b91793ec8ba08e2eed36df865159709e
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-12-13 09:47:34 -0800

    tables added
    .defaultTo(false) attempted

---
+++ data/migrations/20191213090256_projects_table.js
@@ -0,0 +1,86 @@
+
+exports.up = function(knex) {
+    return knex.schema
+    //creating Projects table
+//     A `project` is what needs to be done. We want to store the following data about a `project`:
+
+// - [ ] a unique Id.
+// - [ ] a name. This column is required.
+// - [ ] a description.
+// - [ ] a boolean that indicates if the project has been completed. This column cannot be NULL, the default value should be `false`.
+
+    .createTable("projects", tbl => {
+        tbl.increments();
+
+        tbl.string("name", 255).unique().notNullable();
+
+        tbl.string("description", 255);
+
+        tbl.boolean("completed").defaultTo(false).notNullable();
+    })
+    //creating resource table
+    //A `resource` is anything needed to complete a project, some examples are: a person, a tool, a meeting room or a software license. We want to store the following data about a `resource`:
+// - [ ] a unique Id.
+// - [ ] a name. This column is required.
+// - [ ] a description.
+// The database should not allow resources with duplicate names.
+    .createTable("resources", tbl => {
+        tbl.increments();
+
+        tbl.string("name", 255).unique().notNullable();
+        
+        tbl.string("description", 255)
+    })
+    //creating the tasks table needs to connect through foriegn key to projects table
+//     An `task` one of the steps needed to complete the project. We want to store the following data about an `task`.
+
+// - [ ] a unique id.
+// - [ ] a description of what needs to be done. This column is required.
+// - [ ] a notes column to add additional information.
+// - [ ] a boolean that indicates if the task has been completed. This column cannot be NULL, the default value should be `false`.
+    .createTable("tasks", tbl => {
+        tbl.increments();
+
+        tbl.string("description", 255).notNullable();
+
+        tbl.string("notes",255).notNullable();
+
+        tbl.boolean("completed").defaultTo(false).notNullable()
+
+        //connect task to projects
+        tbl.integer("projet_id")
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("projects")
+    })
+    //project resources table contains connection to proj and resources through foreign ke
+    .createTable("project_resources", tbl => {
+        tbl.increments();
+
+        //project first maybe
+        tbl
+            .integer("project_id")
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .intable("projects")
+
+        //resources
+        tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .intable("resources")
+    })
+};
+
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects")
+};
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const server = express();
 
 server.use(express.json());
 server.use('/api/projects', ProjectRouter);
+
+//sanity check to see if server is running
 server.get('/', (req, res) => {
     res.send('<h2>THE SERVER IS UP AND RUNNING</h2>')
 })
